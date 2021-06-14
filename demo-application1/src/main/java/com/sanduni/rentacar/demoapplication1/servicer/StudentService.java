f146305ccbe5ad03cde6fad1c5d8113a5a0f3e2f
Sanduni93 <nirodhajayaweera1993@gmail.com>
2021-06-15 01:01:39 +0530

    The service provides code snippets to fetch data from the database

--- demo-application1/src/main/java/com/sanduni/rentacar/demoapplication1/servicer/StudentService.java
+++ demo-application1/src/main/java/com/sanduni/rentacar/demoapplication1/servicer/StudentService.java
@@ -1,9 +1,14 @@
 package com.sanduni.rentacar.demoapplication1.servicer;
 
 import com.sanduni.rentacar.demoapplication1.model.Student;
+import com.sanduni.rentacar.demoapplication1.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public interface StudentService {
 
-    Student save(Student  student);
+    Student save(Student student);
 
+    Student fetchStudentById(int id);
 }
